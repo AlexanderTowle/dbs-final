8a31c155b32bbca82447a8f15f5dc5399c956f10
Danna Sandoval Markett <dsandoval@conncoll.edu>
2024-04-29 14:25:52 -0400

    Update dba.py

--- dba.py
+++ dba.py
@@ -5,6 +5,6 @@ Created on Sun Apr 28 23:08:19 2024
 @author: Danna Sandoval
 """
 import mysql.connector
-cnx = mysql.connector.connect(user='root', password='ds8498ds',
-                             host='localhost',
-                             database='com303')
+cnx = mysql.connector.connect(user='com303dsandoval', password='ds8498ds',
+                             host='136.244.224.221',
+                             database='com303fpad')
