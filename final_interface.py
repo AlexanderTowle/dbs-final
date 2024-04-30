8a06520766a05c59eb85421b40a571e93e6f5c0a
AlexanderTowle <144063732+AlexanderTowle@users.noreply.github.com>
2024-04-30 10:34:33 -0400

    Update final_interface.py

--- final_interface.py
+++ final_interface.py
@@ -20,22 +20,44 @@ try:
 
         #Create cursor and query to get the products available at that store
         address_cursor = cnx.cursor()
-        address_query = "select p.product_type from inventory i join product p on (i.product_id = p.product_id) where address_num = %s and street = %s and city = %s and state = %s and zip = %s"
+        address_query = "select p.product_type, p.product_id, i.price from inventory i join product p on (i.product_id = p.product_id) where address_num = %s and street = %s and city = %s and state = %s and zip = %s"
         address_cursor.execute(address_query, (address_num, street, city, state, zip))
 
         #Results
         address_results = address_cursor.fetchall()
+        address_cursor.close()
 
         #Either show products or it isn't a valid store
         if address_results:
-            print("\nProducts available at this location:\n")
+            print("\nProducts available at this location:\n\nProduct   Product ID   Price\n")
             for product in address_results:
-                print(product[0], "\n")
+                print(str(product[0]) + " " + str(product[1]) + " $" + str(product[2]) + "\n")
         else:
             print("\nInvalid store location\n")
             sys.exit()
 
+        #cart holds product_ids 
+        cart = []
+        #Keep track of the total cost
+        total = 0.00
+        print("Please enter the product IDs of the products you would like to purchase one at a time. When you are finished please type \"Checkout\".\n")
+        #Continues prompting until checkout is entered
+        while True:
+            product = input()
+            if product == "Checkout":
+                #Check against the items in the store to fetch the data for what is being bought and add to total cost
+                for item in address_results:
+                    if str(item[1]) in cart:
+                        print(item[2])
+                        total += float(item[2])
+                print("\nYour total is $" + str(total))
+                break
+            #add product_id to cart
+            else:
+                cart.append(product)
+
         
+
 
 #except so we know if we're having trouble connecting (hopefully shouldn't be an issue now that password stuff is figured out)
 except Error as e:
